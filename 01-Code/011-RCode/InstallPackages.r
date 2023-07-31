8c66598a6bf0cc428e99da76bee25d75ba544586
TyphonUK <44297336+TyphonUK@users.noreply.github.com>
2023-07-31 16:10:53 +0100

    Added starts of Age Demographic Methodology Documentation

---
+++ 01-Code/011-RCode/InstallPackages.r
@@ -0,0 +1,8 @@
+# Install Packages
+
+install.packages("knitr")
+install.packages("tidyverse")
+install.packages("sqldf")
+install.packages("magrittr")
+install.packages("readxl")
+install.packages("dplyr")
